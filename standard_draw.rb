e655928bb8ad7be111e972ab4612d356199da707
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-12-27 01:41:34 -0500

    add point

--- standard_draw.rb
+++ standard_draw.rb
@@ -1,60 +1,113 @@
 require 'tk'
 
 class StdDraw
+    BORDER = 0.00
+    DEFAULT_SIZE = 512
+    @@width  = DEFAULT_SIZE
+    @@height = DEFAULT_SIZE
 
-  def initialize
-    @root = TkRoot.new
-    @root.title = "Window"
+    DEFAULT_XMIN = 0.0
+    DEFAULT_XMAX = 1.0
+    DEFAULT_YMIN = 0.0
+    DEFAULT_YMAX = 1.0
+    @@xmin = DEFAULT_XMIN
+    @@ymin = DEFAULT_YMIN
+    @@xmax = DEFAULT_XMAX
+    @@ymax = DEFAULT_YMAX
 
-    @canvas = TkCanvas.new(@root) do
-      place('height' => 170, 'width' => 100, 'x' => 10, 'y' => 10)
-    end
+  DEFAULT_PEN_RADIUS = 0.002
+  @@pen_radius = DEFAULT_PEN_RADIUS
 
-    color = 'blue'
-    TkcOval.new(@canvas, box(5,5,5), outline: color, fill: color)
+  @@color = 'blue'
+
+  @@root = TkRoot.new
+  @@root.title = 'boom'
+  @@canvas = TkCanvas.new(@@root) do
+    height @@height
+    width @@width
+    bg 'white'
   end
+  @@canvas.pack
 
-   def box(x, y, r)
-    [x - r, y - r, x + r, y + r]
-   end
-
-   def self.set_xscale(xmin, xmax)
-   end
-
-   def self.set_yscale(ymin, ymax)
-   end
-
-   def self.set_pen_radius(radius)
-   end
-
-   def self.point(x, y)
-   end
-
-
-  #def point(x,y)
-  #  xs = scale_X x
-  #  ys = scale_Y y
-  #  r = pen_radius
-  #  scaled_pen_radius = r * DEFAULT_SIZE
-
-  #  if scaled_pen_radius <= 1
-  #    pixel(x, y)
-  #  else
-  #      #offscreen.fill(new Ellipse2D.Double(xs - scaledPenRadius/2, ys - scaledPenRadius/2, scaledPenRadius, scaledPenRadius))
-  #  end
-
-  #  draw
-  #end
-
-  #def circle( x, y, r, **kwargs):
-  #  TkcOval.new(@canvas, 9)
-  #  return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
-  #.Canvas.create_circle = _create_circle
-
-  def draw
+  @@flag = false
+  @@thread = Thread.new do
+    @@flag = true
     Tk.mainloop
   end
 
-  def pixel(x, y)
+  def self.box(x, y, r)
+    [x - r, y - r, x + r, y + r]
+  end
+
+   def self.set_xscale(min, max)
+     sleep 1 until @@flag
+     size = max - min
+     throw 'the min and max are the same' if size == 0.0
+     @@xmin = min - BORDER * size
+     @@xmax = max + BORDER * size
+   end
+
+   def self.set_yscale(min, max)
+     sleep 1 until @@flag
+     size = max - min
+     throw 'the min and max are the same' if size == 0.0
+     @@ymin = min - BORDER * size
+     @@ymax = max + BORDER * size
+   end
+
+   def self.pen_radius=(radius)
+     throw 'pen radius must be nonnegative' if radius <= 0
+     @@pen_radius = radius
+   end
+
+   def self.pen_color=(color)
+     @@color = color
+   end
+
+   def self.point(x, y)
+     sleep 1 until @@flag
+
+     xs = scaleX(x)
+     ys = scaleY(y)
+     r = @@pen_radius
+     scaled_pen_radius = r * DEFAULT_SIZE
+
+     if scaled_pen_radius <= 1
+       pixel(x, y)
+     else
+       x1 = xs - scaled_pen_radius/2
+       y1 = ys - scaled_pen_radius/2
+       b = self.box(x1, y1, scaled_pen_radius/2)
+       TkcOval.new(@@canvas, b, outline: @@color, fill: @@color)
+     end
+
+     self.draw
+   end
+
+  def self.pixel(x, y)
+    sleep 1 until @@flag
+    x1 = scaleX(x)
+    x1 = x1.round
+    y1 = scaleY(y)
+    y1 = y1.round
+    TkcRectangle.new(@@canvas, [x1, y1, x1 + 1, y1 + 1], outline: @@color, fill: @@color)
+
+    self.draw
+  end
+
+  def self.scaleX(x)
+    @@width  * (x - @@xmin) / (@@xmax - @@xmin)
+  end
+
+  def self.scaleY(y)
+    @@height * (@@ymax - y) / (@@ymax - @@ymin)
+  end
+
+  def self.draw
+    puts 'draw'
+  end
+
+  def self.final
+    @@thread.join
   end
 end
---
+++ test_point_java.rb
@@ -0,0 +1,8 @@
+#require_relative './standard_draw'
+java_import 'StdDraw'
+
+Java::StdDraw.setPenRadius(0.05);
+Java::StdDraw.setPenColor(Java::StdDraw::BLUE);
+Java::StdDraw.point(0.5, 0.5);
+Java::StdDraw.setPenColor(Java::StdDraw::RED);
+Java::StdDraw.point(0.8, 0.8);
