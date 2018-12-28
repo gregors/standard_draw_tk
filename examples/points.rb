1c4e6aec2ae97df60a928890457ae06b666a8f86
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-12-28 18:27:50 -0500

    add points example

--- examples/points.rb
+++ examples/points.rb
@@ -1,8 +1,8 @@
-require_relative './standard_draw'
+require 'standard_draw_tk'
 
 StdDraw.pen_radius = 0.05
 StdDraw.pen_color = 'blue'
 StdDraw.point(0.5, 0.5)
 StdDraw.pen_color = 'red'
 StdDraw.point(0.8, 0.8)
-StdDraw.final
+StdDraw.pause
