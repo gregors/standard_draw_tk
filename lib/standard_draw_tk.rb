96c758927860c8d2edfa06a3fbd5eb7a9ed04ce1
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2018-12-28 17:42:11 -0500

    dry up code

--- lib/standard_draw_tk.rb
+++ lib/standard_draw_tk.rb
@@ -80,20 +80,7 @@ class StdDraw
   end
 
   def self.point(x, y)
-    xs = coords.scale_x(x)
-    ys = coords.scale_y(y)
-
-    r = @@pen_radius
-    scaled_pen_radius = r * StandardDrawTk::Coordinates::DEFAULT_SIZE
-
-    if scaled_pen_radius <= 1
-      pixel(x, y)
-    else
-      x1 = xs - scaled_pen_radius/2
-      y1 = ys - scaled_pen_radius/2
-      b = coords.box(x1, y1, scaled_pen_radius/2)
-      TkcOval.new(@@canvas, b, outline: color, fill: color)
-    end
+    draw_circle(x, y, @@pen_radius, outline: color, fill: color)
   end
 
   def self.pixel(x, y)
